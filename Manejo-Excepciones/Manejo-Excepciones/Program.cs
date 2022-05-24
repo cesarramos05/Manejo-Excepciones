ac235b4692fe0b4d0fe81aa7f405938a4f7821c5
cesarramos05 <43106466+cesarramos05@users.noreply.github.com>
2022-05-24 11:48:05 -0700

    C#

---
+++ Manejo-Excepciones/Manejo-Excepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejo_Excepciones
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Variables 
+                int num;
+                Console.WriteLine("Programa que eleva al cuadrado un numero");
+                Console.Write("Ingrese numero entero a calcular: ");
+                num = int.Parse(Console.ReadLine());
+                
+                //Impresion y calculo
+                Console.WriteLine("\nEl numero " + num + " al cuadrado es igual a: " + Math.Pow(num, 2));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nNo cumple con el formato adecuado");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("\nEl numero es muy grande");
+            }
+            Console.WriteLine("Ingrese ENTER para salir");
+            Console.ReadKey();
+
+        }
+    }
+}
